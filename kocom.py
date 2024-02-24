844649eb884135282cb29a875d4537a3d50fa7b2
colddays500 <55190650+colddays500@users.noreply.github.com>
2024-02-24 16:58:27 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -23,7 +23,7 @@ import configparser
 
 
 # define -------------------------------
-SW_VERSION = '2024.02.001'
+SW_VERSION = '2024.02.002'
 CONFIG_FILE = 'kocom.conf'
 BUF_SIZE = 100
 
@@ -51,17 +51,17 @@ room_h_dic = {'livingroom':'00', 'myhome':'00', 'room1':'01', 'room2':'02', 'roo
 # mqtt functions ----------------------------
 
 def init_mqttc():
-    mqttc = mqtt.Client()
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
     mqttc.on_message = mqtt_on_message
     mqttc.on_subscribe = mqtt_on_subscribe
     mqttc.on_connect = mqtt_on_connect
     mqttc.on_disconnect = mqtt_on_disconnect
 
-    if config.get('MQTT','mqtt_allow_anonymous') != 'True':
+#    if config.get('MQTT','mqtt_allow_anonymous') != 'True':
         logtxt = "[MQTT] connecting (using username and password)"
         mqttc.username_pw_set(username=config.get('MQTT','mqtt_username',fallback=''), password=config.get('MQTT','mqtt_password',fallback=''))
-    else:
-        logtxt = "[MQTT] connecting (anonymous)"
+#    else:
+#        logtxt = "[MQTT] connecting (anonymous)"
 
     mqtt_server = config.get('MQTT','mqtt_server')
     mqtt_port = int(config.get('MQTT','mqtt_port'))
