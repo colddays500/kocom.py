b781ba6188362e4e9806c5c10da739cd2caf480c
colddays500 <55190650+colddays500@users.noreply.github.com>
2024-02-24 17:12:38 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -66,7 +66,7 @@ def init_mqttc():
 #        logtxt = "[MQTT] connecting (anonymous)"
 #    mqtt_server = config.get('MQTT','mqtt_server')
 #    mqtt_port = int(config.get('MQTT','mqtt_port'))
-    mqtt_server = 192.168.0.2
+    mqtt_server = '192.168.0.2'
     mqtt_port = 1883
     for retry_cnt in range(1,31):
         try:
